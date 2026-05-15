35394dc60222ccdbf3dad52afb2b39b0d37a7f24
pangeaexp <154689388+pangeaexp@users.noreply.github.com>
2026-05-15 21:05:53 +0200

    Update dependencies and enforce TypeScript build

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,6 @@ const nextConfig = {
     NEXT_PUBLIC_CEJTT_SUPABASE_URL:
       process.env.NEXT_PUBLIC_CEJTT_SUPABASE_URL || process.env.CEJTT_SUPABASE_URL,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true,
   },
